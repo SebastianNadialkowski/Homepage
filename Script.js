e25afa5c312572bb1fd0af8e37b3b5a5085a1a53
Sebastian Nadiałkowski <Sirskuty@gmail.com>
2023-01-16 17:55:58 +0100

    refactored  projekt kantor Js

--- Script.js
+++ Script.js
@@ -15,43 +15,42 @@
     doIt();
 }
 
-let valueElement = document.querySelector(".js-value");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
-
-let rateEUR = 4.6843;
-let rateUSD = 4.4100;
-let rateGBP = 5.4418;
-let rateCHF = 4.7482;
-let rateJPY = 3.2291;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let value = +valueElement.value;
-    let currency = currencyElement.value;
-    let result;
-
-    switch (currency) {
-        case "EUR":
-            result = value / rateEUR;
-            break;
-
-        case "USD":
-            result = value / rateCHF;
-            break;
-
-        case "GBP":
-            result = value / rateGBP;
-            break;
-
-        case "CHF":
-            result = value / rateCHF;
-            break;
-
-        case "JPY":
-            result = value / rateJPY;
-
+{
+    const calculateResult = (value, currency) => {
+        const rateEUR = 4.6843;
+        const rateUSD = 4.4100;
+        const rateGBP = 5.4418;
+        const rateCHF = 4.7482;
+        const rateJPY = 3.2291;
+        switch (currency) {
+            case "EUR":
+                return value / rateEUR;
+            case "USD":
+                return value / rateUSD;
+            case "GBP":
+                return value / rateGBP;
+            case "CHF":
+                return value / rateCHF;
+            case "JPY":
+                return value / rateJPY;
+        }
     }
-    resultElement.innerText = `${value.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-});
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const valueElement = document.querySelector(".js-value");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+        const value = +valueElement.value;
+        const currency = currencyElement.value
+        const result = calculateResult(value, currency);
+        resultElement.innerText = `${value.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+    }
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+    init();
+}
+
+
